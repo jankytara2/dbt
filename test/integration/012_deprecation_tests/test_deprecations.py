0f4a84642f4afd461487b999298310f473e539a2
Emily Rockman <emily.rockman@dbtlabs.com>
2021-10-04 15:03:48 -0500

    Change the default dbt packages installation directory to `dbt_packages` from `dbt_modules`.  Also rename `module-path` to `packages-install-path` to allow default overrides of package install directory.  Deprecation warning added for projects using the old `dbt_modules` name without specifying a `packages-install-path`.

--- test/integration/012_deprecation_tests/test_deprecations.py
+++ test/integration/012_deprecation_tests/test_deprecations.py
@@ -73,6 +73,33 @@ class TestDispatchPackagesDeprecation(BaseTestDeprecations):
         assert 'Raised during dispatch for: string_literal' in exc_str
 
 
+class TestPackageInstallPathDeprecation(BaseTestDeprecations):
+    @property
+    def models(self):
+        return self.dir('dispatch-models')
+
+    @property
+    def project_config(self):
+        return {
+            'config-version': 2,
+            "clean-targets": "dbt_modules"
+        }
+
+    @use_profile('postgres')
+    def test_postgres_adapter_macro(self):
+        self.assertEqual(deprecations.active_deprecations, set())
+        self.run_dbt(["clean"])
+        expected = {'install-packages-path'}
+        self.assertEqual(expected, deprecations.active_deprecations)
+
+    @use_profile('postgres')
+    def test_postgres_adapter_macro_fail(self):
+        self.assertEqual(deprecations.active_deprecations, set())
+        with self.assertRaises(dbt.exceptions.CompilationException) as exc:
+            self.run_dbt(['--warn-error', 'run'])
+        exc_str = ' '.join(str(exc.exception).split())  # flatten all whitespace
+        assert 'path has changed from `dbt_modules` to `dbt_packages`.' in exc_str
+
 class TestPackageRedirectDeprecation(BaseTestDeprecations):
     @property
     def models(self):
